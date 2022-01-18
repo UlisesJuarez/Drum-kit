c7f5d2226c7c8ebc3f9f186e8baaae1e06038f60
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-01-17 23:05:49 -0600

    add images to buttons

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 var total_botones=document.querySelectorAll(".drum").length;
+
 for(var i=0;i<total_botones;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function (){
-
-        alert("I got click")
+        var audio=new Audio("sounds/tom-2.mp3");
+        audio.play();
     });
 }
+
